949c637c7b6a30fad1acc382bbe663e9c4015c21
Joaquin Diaz Ramirez <joaquin.diaz.ramirez.8897@gmail.com>
2018-11-29 09:22:06 +0100

    Subida del Proyecto Material10. Ejercicio que acabar en casa.

---
+++ app/src/main/java/matinal/example/com/material10/Material10.java
@@ -0,0 +1,13 @@
+package matinal.example.com.material10;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Material10 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_material10);
+    }
+}
